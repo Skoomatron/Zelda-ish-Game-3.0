775ef8d93a54532ef4143b0acb9c600818be7a36
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-08 14:41:16 -0400

    Modified RoomTransfer Script To Check/Display Area Name Text

--- Assets/Scripts/RoomTransfer.cs
+++ Assets/Scripts/RoomTransfer.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomTransfer : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+
 
     void Start()
     {
@@ -25,6 +31,18 @@ public class RoomTransfer : MonoBehaviour
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
+            if(needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
     }
 }
