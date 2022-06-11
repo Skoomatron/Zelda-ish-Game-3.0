1691802065030a2afdcc84e3ac7010cdc64db03a
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-11 07:59:10 -0400

    Enemy Script Updated - Basic Orc Created and Linked

--- Assets/Scripts/BasicOrc.cs
+++ Assets/Scripts/BasicOrc.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class BasicOrc : Enemy
 {
-    public int health;
-    public string enemyName;
-    public int baseAttack;
-    public float moveSpeed;
     void Start()
     {
 
