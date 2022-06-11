aac2fd06ad45cb1e342d91cae9bc4ef0068ff9de
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-11 08:05:01 -0400

    Added Proximity Check And Movement To BasicOrc Script

--- Assets/Scripts/BasicOrc.cs
+++ Assets/Scripts/BasicOrc.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class BasicOrc : Enemy
 {
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
     void Start()
     {
-
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     void Update()
     {
+        CheckDistance();
+    }
 
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, attackRadius);
+        }
     }
 }
