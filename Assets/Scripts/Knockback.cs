3fb5c00fdec10b8fd8c0f4dcb40d18fc6167cec3
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-15 09:15:58 -0400

    Added If Condition To Initiate Player Knockback Coroutine

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -17,14 +17,19 @@ public class Knockback : MonoBehaviour
             Rigidbody2D hit = collision.GetComponent<Rigidbody2D>();
             if (hit != null)
             {
+                Vector2 differential = hit.transform.position - transform.position;
+                differential = differential.normalized * thrust;
+                hit.AddForce(differential, ForceMode2D.Impulse);
                 if (collision.gameObject.CompareTag("Enemies"))
                 {
                     hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
                     collision.GetComponent<Enemy>().Knock(hit, knockTime);
                 }
-                Vector2 differential = hit.transform.position - transform.position;
-                differential = differential.normalized * thrust;
-                hit.AddForce(differential, ForceMode2D.Impulse);
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
+                    collision.GetComponent<PlayerMovement>().Knock(knockTime);
+                }
             }
         }
     }
