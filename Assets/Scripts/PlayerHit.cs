faf4065eb035b1656df272c231c3a8812ba049da
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-17 06:09:52 -0400

    Removed Start And Update From Player Hit

--- Assets/Scripts/PlayerHit.cs
+++ Assets/Scripts/PlayerHit.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class PlayerHit : MonoBehaviour
 {
-
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Breakable"))
