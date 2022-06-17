abe9df60b5c50bce30e8b7c0dc060343413c47af
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-17 06:33:51 -0400

    Added Check For Idle In Player Movement - Stops Getting Glued To Enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -36,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
         {
             StartCoroutine(AttackCo());
         }
-        else if (currentState == PlayerState.walk)
+        else if (currentState == PlayerState.walk || currentState == PlayerState.idle)
         {
             UpdateAnimationAndMove();
         }
