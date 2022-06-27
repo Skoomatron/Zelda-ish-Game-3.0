3a3dee081e85c3589f3863169cbaf19937e24fcd
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-27 07:56:58 -0400

    Fixed Issue With Losing Main Starting Position

--- Assets/Scripts/ScriptableObjects/VectorValue.cs
+++ Assets/Scripts/ScriptableObjects/VectorValue.cs
@@ -2,7 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
-public class VectorValue : ScriptableObject
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public Vector2 initialValue;
+    public Vector2 defaultValue;
+    public void OnAfterDeserialize()
+    {
+        initialValue = defaultValue;
+    }
+    public void OnBeforeSerialize()
+    {
+
+    }
 }
