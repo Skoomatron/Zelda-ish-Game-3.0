b6be3af384b0dde56894b501c0a43c1ab8334466
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-27 09:46:34 -0400

    Changes To Fade Transitions

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,12 +8,37 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue storedPosition;
+    public GameObject fadeIn;
+    public GameObject fadeOut;
+    public float fadeWait;
+    private void Awake()
+    {
+        if(fadeIn != null)
+        {
+            GameObject panel = Instantiate(fadeIn, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger)
         {
             storedPosition.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+        }
+    }
+
+    private IEnumerator FadeCo()
+    {
+        if (fadeOut != null)
+        {
+            Instantiate(fadeOut, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
         }
     }
 }
