b43a6925b595a2bd2e4b4b395f5346da2a86e3f5
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-28 14:10:10 -0400

    Work On Chest Script

--- Assets/Scripts/Objects/BasicChest.cs
+++ Assets/Scripts/Objects/BasicChest.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BasicChest : Interactables
 {
-    // Start is called before the first frame update
+    public Item contents;
+    public Inventory playerInventory;
+    public bool isOpen;
+    public SignalClass raiseItem;
+    public GameObject dialogBox;
+    public Text dialogText;
+    private Animator anim;
     void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (!isOpen)
+            {
+                OpenChest();
+            } else {
+                ChestOpened();
+            }
+        }
+    }
+    public void OpenChest()
     {
 
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ChestOpened()
     {
 
     }
