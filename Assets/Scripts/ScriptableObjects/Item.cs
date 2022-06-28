be0305f4e238252df4e3dde6389465a33da9227b
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-28 10:47:45 -0400

    Created Scriptable Object For Random Fantastic Item - Is keyObject

--- Assets/Scripts/ScriptableObjects/Item.cs
+++ Assets/Scripts/ScriptableObjects/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class Inventory : ScriptableObject
+public class Item : ScriptableObject
 {
     public Sprite itemSprite;
     public string itemName;
