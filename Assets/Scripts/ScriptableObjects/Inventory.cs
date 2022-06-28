6dff7d4d3bfa258cf94e5a3f7fb14ff36146c2e0
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-28 10:36:44 -0400

    Created New Scriptables for Item Management

--- Assets/Scripts/ScriptableObjects/Inventory.cs
+++ Assets/Scripts/ScriptableObjects/Inventory.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Inventory : MonoBehaviour
+[CreateAssetMenu]
+public class Inventory : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Item currentItem;
+    public List<Item> items = new List<Item>();
+    public int numberOfKeyItems;
 }
