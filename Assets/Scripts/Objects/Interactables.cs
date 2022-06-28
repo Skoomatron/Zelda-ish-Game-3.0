93183d22b9f5b76c2d0659ae80abb77c8b33493e
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-28 09:09:56 -0400

    Parent Class Created For All Interactables

--- Assets/Scripts/Objects/Interactables.cs
+++ Assets/Scripts/Objects/Interactables.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Interactables : MonoBehaviour
 {
+     public SignalClass context;
+    public bool playerInRange;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            context.Raise();
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D (Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            context.Raise();
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/Objects/Sign.cs
+++ Assets/Scripts/Objects/Sign.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sign : MonoBehaviour
+public class Sign : Interactables
 {
-    public SignalClass context;
     public GameObject dialogBox;
     public Text dialogText;
     public string dialog;
-    public bool playerInRange;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
@@ -21,23 +19,6 @@ public class Sign : MonoBehaviour
                 dialogBox.SetActive(true);
                 dialogText.text = dialog;
             }
-        }
-    }
-    private void OnTriggerEnter2D (Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            context.Raise();
-            playerInRange = true;
-        }
-    }
-    private void OnTriggerExit2D (Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            context.Raise();
-            playerInRange = false;
-            dialogBox.SetActive(false);
         }
     }
 }
