d67521bba33032659dc833a726007bce5f6a753f
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-01 09:56:24 -0400

    Player Added To Dungeon

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorType
+{
+    key,
+    enemy,
+    button
+}
+public class Door : Interactables
+{
+    [Header("Door Variables")]
+    public DoorType thisDoorType;
+    public bool open = false;
+}
