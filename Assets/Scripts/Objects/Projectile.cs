f53ac30f4b56961c3fea37637827951020155cd2
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-07 10:29:39 -0400

    Updating Projectile Values In Unity

--- Assets/Scripts/Objects/Projectile.cs
+++ Assets/Scripts/Objects/Projectile.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float damage;
-    public float knockBack;
+    [Header("Projectile Parameters")]
+    public float projectileSpeed;
+    public Vector2 directionToMove;
+    [Header("Projectile Duration")]
+    public float lifetime;
+    private float decayTime;
+    private Rigidbody2D myRigidBody;
     void Start()
     {
-
+        decayTime = lifetime;
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        decayTime -= Time.deltaTime;
+        if (decayTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
+    public void Launch(Vector2 initialDestination)
+    {
+        myRigidBody.velocity = initialDestination * projectileSpeed;
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
     }
 }
