6c38dd2aea6acf6f46dc079d0d9fcae72a5fd1b2
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-07 10:14:06 -0400

    Projectile Groundwork

---
+++ Assets/Scripts/Objects/Projectile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+    public float knockBack;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
