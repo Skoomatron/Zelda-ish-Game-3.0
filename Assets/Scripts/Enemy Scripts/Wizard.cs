2910fb858eb6d49d8664db08654fe9048cc5136a
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-07 11:01:38 -0400

    Adjusting Projectile Delay

--- Assets/Scripts/Enemy Scripts/Wizard.cs
+++ Assets/Scripts/Enemy Scripts/Wizard.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class Wizard : BasicOrc
 {
     public GameObject projectile;
+    public float fireDelay;
+    private float fireDelaySeconds;
+    public bool canFire = true;
+    private void update()
+    {
+        fireDelaySeconds -= Time.deltaTime;
+        if (fireDelaySeconds <= 0)
+        {
+            canFire = true;
+            fireDelaySeconds = fireDelay;
+        }
+    }
     public override void CheckDistance()
     {
         if (Vector3.Distance
@@ -15,16 +27,21 @@ public class Wizard : BasicOrc
                 if (currentState == EnemyState.idle || currentState == EnemyState.walk
                 && currentState != EnemyState.stagger)
                 {
-                    Vector3 tempVector = transform.position - target.transform.position;
-                    GameObject currentProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
-                    currentProjectile.GetComponent<Projectile>().Launch(tempVector);
-                    ChangeState(EnemyState.walk);
-                    animator.SetBool("Moving", true);
+                    if (canFire)
+                    {
+                        Vector3 tempVector = target.transform.position - transform.position;
+                        GameObject currentProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+                        currentProjectile.GetComponent<Projectile>().Launch(tempVector);
+                        canFire = false;
+                        ChangeState(EnemyState.walk);
+                        animator.SetBool("Moving", true);
+                    }
                 }
                 else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
                 {
                     animator.SetBool("Moving", false);
                 }
             }
+
     }
 }
