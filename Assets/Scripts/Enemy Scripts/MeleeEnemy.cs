5d6fc025b3ab6390251798778d872dc7a20df18c
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-13 12:33:47 -0400

    Ogre Attack Animation Working

--- Assets/Scripts/Enemy Scripts/MeleeEnemy.cs
+++ Assets/Scripts/Enemy Scripts/MeleeEnemy.cs
@@ -15,4 +15,35 @@ public class MeleeEnemy : BasicOrc
     {
 
     }
+
+    public override void CheckDistance()
+    {
+        if (Vector3.Distance
+            (target.position, transform.position) <= chaseRadius
+            && Vector3.Distance(target.position, transform.position) > attackRadius
+        )
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk
+            && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+                animator.SetBool("Moving", true);
+            }
+        } else if (Vector3.Distance(target.position, transform.position) <= chaseRadius
+                && Vector3.Distance(target.position, transform.position) <= attackRadius)
+                {
+                    StartCoroutine(AttackCo());
+                }
+    }
+    public IEnumerator AttackCo()
+    {
+        currentState = EnemyState.attack;
+        animator.SetBool("Attacking", true);
+        yield return new WaitForSeconds(0.5f);
+        currentState = EnemyState.walk;
+        animator.SetBool("Attacking", false);
+    }
 }
