1103ca1d02735e7d04a1280c13d4db27f430f8e8
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-16 07:58:48 -0400

    Adding Wait/Move Times To Border NPC to Better Randomize Movement

--- Assets/Scripts/NPC Scripts/BoundedNPC.cs
+++ Assets/Scripts/NPC Scripts/BoundedNPC.cs
@@ -8,10 +8,19 @@ public class BoundedNPC : Interactables
     private Transform myTransform;
     private Rigidbody2D myRigidbody;
     private Animator animator;
+    private bool moving;
+    [Header("Boundary Area")]
     public Collider2D bounds;
+    [Header("Move Variables")]
     public float speed;
+    public float moveTime;
+    public float waitTime;
+    private float moveDecay;
+    private float waitDecay;
     void Start()
     {
+        waitDecay = waitTime;
+        moveDecay = moveTime;
         animator = GetComponent<Animator>();
         myTransform = GetComponent<Transform>();
         myRigidbody = GetComponent<Rigidbody2D>();
@@ -19,9 +28,27 @@ public class BoundedNPC : Interactables
     }
     void Update()
     {
-        if(!playerInRange)
+        if (moving)
         {
-            NPCMove();
+            moveDecay -= Time.deltaTime;
+            if (moveDecay <= 0)
+            {
+                moveDecay = moveTime;
+                moving = false;
+            }
+            if(!playerInRange)
+            {
+                NPCMove();
+            }
+        }
+        else
+        {
+            waitDecay -= Time.deltaTime;
+            if (waitDecay <= 0)
+            {
+                moving = true;
+                waitDecay = waitTime;
+            }
         }
     }
     void UpdateAnimation()
