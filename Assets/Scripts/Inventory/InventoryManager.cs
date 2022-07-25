99fc01e70518e1a4c154667ed62e21b93a3c8fe6
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-25 16:01:35 -0400

    Fixed GameObject Typo

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     [Header("Inventory Information")]
-    [SerializeField] private GameOject blankInventorySlot;
+    [SerializeField] private GameObject blankInventorySlot;
     [SerializeField] private GameObject inventoryPanel;
     [SerializeField] private TextMeshProUGUI descriptionText;
     [SerializeField] private GameObject useButton;
