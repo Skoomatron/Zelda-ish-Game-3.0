14a81ae2dee5cba50b5e010aae4dafd221359141
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-25 15:12:44 -0400

    Setup Method Created - Walked Back Some Value Declarations

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -10,11 +10,19 @@ public class InventorySlot : MonoBehaviour
     [SerializeField] private TextMeshProUGUI itemNumberText;
     [SerializeField] private Image itemImage;
     [Header("Item Variables")]
-    public Sprite itemSprite;
-    public int numberHeld;
-    public string itemDescription;
     public InventoryItem thisItem;
     public InventoryManager thisManager;
+
+    public void Setup(InventoryItem newItem, InventoryManager newManager)
+    {
+        thisItem = newItem;
+        thisManager = newManager;
+        if (thisItem)
+        {
+            itemImage.sprite = thisItem.itemImage;
+            itemNumberText.text = "" + thisItem.numberHeld;
+        }
+    }
     void Start()
     {
 
