d6e32e88a38be923886870f2bf8bed3e44b08f93
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-27 07:07:42 -0400

    Potion Scripts Created

---
+++ Assets/Scripts/Inventory/MagicReaction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicReaction : MonoBehaviour
+{
+    public FloatValue playerMagic;
+    public SignalClass magicSignal;
+    public void Use(int amountToIncrease)
+    {
+        playerMagic.RuntimeValue += amountToIncrease;
+        magicSignal.Raise();
+    }
+}
