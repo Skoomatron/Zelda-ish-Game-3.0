43d512e6ceaf5278367e19b3d0bea9ce93838277
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-31 05:58:49 -0400

    Copied Some Methods From Game Saver

--- Assets/Scripts/Inventory/InventorySaver.cs
+++ Assets/Scripts/Inventory/InventorySaver.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class InventorySaver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private PlayerInventory myInventory;
+    public void ResetScriptables() {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.dat", i)))
+            {
+                File.Delete(Application.persistentDataPath + string.Format("/{0}.dat", i));
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        LoadScriptables();
+    }
+    private void OnDisable()
+    {
+        SaveScriptables();
+    }
+    public void SaveScriptables()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}.dat", i));
+            BinaryFormatter binary = new BinaryFormatter();
+            var json = JsonUtility.ToJson(objects[i]);
+            binary.Serialize(file, json);
+            file.Close();
+        }
+    }
+    public void LoadScriptables()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.dat", i)))
+            {
+                FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}.dat", i), FileMode.Open);
+                BinaryFormatter binary = new BinaryFormatter();
+                JsonUtility.FromJsonOverwrite((string)binary.Deserialize(file), objects[i]);
+                file.Close();
+            }
+        }
     }
 }
