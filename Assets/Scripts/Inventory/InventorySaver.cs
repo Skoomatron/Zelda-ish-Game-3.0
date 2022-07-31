ae41a002d7d1c3dea3d72384bf8ed22213d58926
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-07-31 06:02:38 -0400

    Added Using IO/Binary Formatters To Item Saver

--- Assets/Scripts/Inventory/InventorySaver.cs
+++ Assets/Scripts/Inventory/InventorySaver.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class InventorySaver : MonoBehaviour
 {
     [SerializeField] private PlayerInventory myInventory;
-    public void ResetScriptables() {
-        for (int i = 0; i < objects.Count; i++)
-        {
-            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.dat", i)))
-            {
-                File.Delete(Application.persistentDataPath + string.Format("/{0}.dat", i));
-            }
-        }
-    }
     private void OnEnable()
     {
         LoadScriptables();
@@ -22,26 +15,35 @@ public class InventorySaver : MonoBehaviour
     {
         SaveScriptables();
     }
+    public void ResetScriptables() {
+        for (int i = 0; i < myInventory.myInventory.Count; i++)
+        {
+            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.inv", i)))
+            {
+                File.Delete(Application.persistentDataPath + string.Format("/{0}.inv", i));
+            }
+        }
+    }
     public void SaveScriptables()
     {
-        for (int i = 0; i < objects.Count; i++)
+        for (int i = 0; i < myInventory.myInventory.Count; i++)
         {
-            FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}.dat", i));
+            FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}.inv", i));
             BinaryFormatter binary = new BinaryFormatter();
-            var json = JsonUtility.ToJson(objects[i]);
+            var json = JsonUtility.ToJson(myInventory[i]);
             binary.Serialize(file, json);
             file.Close();
         }
     }
     public void LoadScriptables()
     {
-        for (int i = 0; i < objects.Count; i++)
+        for (int i = 0; i < myInventory.myInventory.Count; i++)
         {
-            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.dat", i)))
+            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.inv", i)))
             {
-                FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}.dat", i), FileMode.Open);
+                FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}.inv", i), FileMode.Open);
                 BinaryFormatter binary = new BinaryFormatter();
-                JsonUtility.FromJsonOverwrite((string)binary.Deserialize(file), objects[i]);
+                JsonUtility.FromJsonOverwrite((string)binary.Deserialize(file), myInventory[i]);
                 file.Close();
             }
         }
