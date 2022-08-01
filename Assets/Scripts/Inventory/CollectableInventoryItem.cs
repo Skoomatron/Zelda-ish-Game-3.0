d612c8b7b0f0a496547c576f5dbd83c46142889c
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-01 06:48:24 -0400

    Abstracting Health Mechanics to Generic Components

--- Assets/Scripts/Inventory/CollectableInventoryItem.cs
+++ Assets/Scripts/Inventory/CollectableInventoryItem.cs
@@ -24,7 +24,7 @@ public class CollectableInventoryItem : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")  && !collision.isTrigger1)
+        if (collision.gameObject.CompareTag("Player")  && !collision.isTrigger)
         {
             AddItemToInventory();
             Destroy(this.gameObject);
