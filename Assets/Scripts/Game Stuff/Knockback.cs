30a49e379041c0542401930c9cf09ba4aec4b494
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-02 04:50:23 -0400

    Implemented DoTween On Knockback

--- Assets/Scripts/Game Stuff/Knockback.cs
+++ Assets/Scripts/Game Stuff/Knockback.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Knockback : MonoBehaviour
 {
@@ -21,9 +22,9 @@ public class Knockback : MonoBehaviour
             Rigidbody2D hit = collision.GetComponent<Rigidbody2D>();
             if (hit != null)
             {
-                Vector2 differential = hit.transform.position - transform.position;
+                Vector3 differential = hit.transform.position - transform.position;
                 differential = differential.normalized * thrust;
-                hit.AddForce(differential, ForceMode2D.Impulse);
+                hit.DOMove(hit.transform.position + differential, knockTime);
                 if (collision.gameObject.CompareTag("Enemies") && collision.isTrigger)
                 {
                     hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
