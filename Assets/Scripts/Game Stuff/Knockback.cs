31aecfe7a823a1338f39d5e68905e471252de7ec
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-02 04:37:51 -0400

    Beginning Refactors to Knockback Script

--- Assets/Scripts/Game Stuff/Knockback.cs
+++ Assets/Scripts/Game Stuff/Knockback.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Knockback : MonoBehaviour
 {
     [Header("Knockback Parameters")]
-    public float thrust;
-    public float knockTime;
-    [Header("Damage Variable")]
-    public float damage;
+    [SerializeField] private float thrust;
+    [SerializeField] private float knockTime;
+    [SerializeField] private string collisionTag;
+    // [Header("Damage Variable")]
+    // public float damage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Breakable") && this.gameObject.CompareTag("Player"))
-        {
-            collision.GetComponent<PotSmashing>().Smash();
-        }
-        if (collision.gameObject.CompareTag("Enemies") || collision.gameObject.CompareTag("Player"))
+        // if (collision.gameObject.CompareTag("Breakable") && this.gameObject.CompareTag("Player"))
+        // {
+        //     collision.GetComponent<PotSmashing>().Smash();
+        // }
+        if (collision.gameObject.CompareTag(collisionTag))
         {
             Rigidbody2D hit = collision.GetComponent<Rigidbody2D>();
             if (hit != null)
@@ -26,14 +27,14 @@ public class Knockback : MonoBehaviour
                 if (collision.gameObject.CompareTag("Enemies") && collision.isTrigger)
                 {
                     hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
-                    collision.GetComponent<Enemy>().Knock(hit, knockTime, damage);
+                    collision.GetComponent<Enemy>().Knock(hit, knockTime);
                 }
                 if (collision.gameObject.CompareTag("Player"))
                 {
                     if (collision.GetComponent<PlayerMovement>().currentState != PlayerState.stagger)
                     {
                         hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
-                        collision.GetComponent<PlayerMovement>().Knock(knockTime, damage);
+                        collision.GetComponent<PlayerMovement>().Knock(knockTime);
                     }
                 }
             }
